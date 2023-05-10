479e6eda0658b2bedc7111eb676110ad0e558df1
Luong The Hai <hailuongthe2000@gmail.com>
2023-05-10 10:15:21 +0700

    test

--- server.py
+++ server.py
@@ -2,7 +2,22 @@ import zmq
 import time
 import sys
 
+import http.server
+import socketserver
+import threading
+
+def test(port):
+    handler = http.server.SimpleHTTPRequestHandler
+    
+    with socketserver.TCPServer(("", PORT), handler) as httpd:
+        print("Server started at localhost:" + str(PORT))
+        httpd.serve_forever()
+
+PORT = 8000
+
+
 port = "5556"
+
 if len(sys.argv) > 1:
     port = sys.argv[1]
     int(port) # make sure port is number
@@ -11,6 +26,9 @@ context = zmq.Context()
 socket = context.socket(zmq.REP)
 print("Binding from tcp://*:%s" % port)
 socket.bind("tcp://*:%s" % port)
+
+t = threading.Thread(target=test, args=(PORT,))
+t.start()
 
 while True:
     message = str(socket.recv().decode())
